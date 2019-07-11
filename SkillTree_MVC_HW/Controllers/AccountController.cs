9895a085a58d84f9443264451d642a245677b794
brian90191 <brian90191@gmail.com>
2019-07-11 08:22:46 +0800

    1. 新增 ViewModal 並定義記帳清單格式 2. 新增 AccountControler 作為記帳清單使用 3. 於 Account 目錄下新增 Index 頁面並導入 Partial View呈現記帳清單及新增表單 4. 於 Views/Shard 下新增記帳清單及新增表單的 Partial View

---
+++ SkillTree_MVC_HW/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkillTree_MVC_HW.Models.ViewModels;
+
+namespace SkillTree_MVC_HW.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Random rnd = new Random();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            List<AccountViewModel> AccountList = new List<AccountViewModel>();
+
+            string[] types = { "支出", "收入" };
+            for (int i = 0; i < 100; i++)
+            {
+                int typeIndex = rnd.Next(2);
+                int rndMoney = rnd.Next(1, 10000);
+
+                var AccountResult = new AccountViewModel()
+                {
+                    Type = types[typeIndex],
+                    DT = RandomDay(),
+                    Total = rndMoney
+                };
+
+                AccountList.Add(AccountResult);
+            }
+
+            return View(AccountList);
+        }
+
+        DateTime RandomDay()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(rnd.Next(range));
+        }
+    }
+}
---
+++ SkillTree_MVC_HW/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace SkillTree_MVC_HW.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string Type { get; set; }
+        public DateTime DT { get; set; }
+        public int Total { get; set; }
+    }
+}
